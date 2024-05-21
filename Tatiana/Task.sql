7151d24088e4abdd4228ccbe0b8fcaf232685c9b
Tat <151627082+SuesmairT@users.noreply.github.com>
2024-05-21 13:29:04 +0200

    Update Task.sql

--- Tatiana/Task.sql
+++ Tatiana/Task.sql
@@ -3,7 +3,7 @@ CREATE TABLE products (
 	created_at timestamp not NULL default current_timestamp,
     title varchar(240) not null,
 	price int not null,
-  	quantity int not null
+  	quantity int not null)
 
   	INSERT INTO products (title, price, quantity)
     VALUES
