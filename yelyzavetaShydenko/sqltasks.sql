3d4d73a590ce520ed407021757f827f76ac6e667
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-05-21 14:05:00 +0200

    V2

--- yelyzavetaShydenko/sqltasks.sql
+++ yelyzavetaShydenko/sqltasks.sql
@@ -10,3 +10,9 @@ INSERT INTO products (created_at, title, price, quantity)
 VALUES
 	(current_timestamp(), 'Visa card', 50, 1),
    (current_timestamp(), 'Mastercard card', 100, 1)
+
+   SELECT currency, sum(amount) as total_sum FROM txs
+join accounts on txs.sender_account_id = accounts.id
+group by currency
+order by total_sum DESC
+limit 1
