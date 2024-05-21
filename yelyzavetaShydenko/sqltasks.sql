ec409b60c3779037f34f3c4683e8e7a2cf16d487
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-05-21 14:06:34 +0200

    V2

--- yelyzavetaShydenko/sqltasks.sql
+++ yelyzavetaShydenko/sqltasks.sql
@@ -11,7 +11,7 @@ VALUES
 	(current_timestamp(), 'Visa card', 50, 1),
    (current_timestamp(), 'Mastercard card', 100, 1)
 
-   SELECT currency, sum(amount) as total_sum FROM txs
+SELECT currency, sum(amount) as total_sum FROM txs
 join accounts on txs.sender_account_id = accounts.id
 group by currency
 order by total_sum DESC
